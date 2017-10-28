95a023b048bf3a7536178afc48833c3781008d0a
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-10-28 19:33:37 +0900

    diff

--- app/webFront/lover-duck-web-front/src/components/LP.vue
+++ app/webFront/lover-duck-web-front/src/components/LP.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="lp">
+    <img class="logo" src="../assets/loverduck_logo.svg" />
   </div>
 </template>
 <script>
 </script>
 <style scoped>
 .lp {
-  float: left;
-  width: 100vh;
+  width: 100vw;
+  height: 150vh;
+  background-image: url("../assets/background.jpg");
+}
+
+.logo {
+  margin: 10% 35%;
+  width: 30%;
 }
 </style>
