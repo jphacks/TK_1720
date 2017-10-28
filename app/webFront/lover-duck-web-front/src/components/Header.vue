61908ada69d698e92ecf3db635e033739e8057d6
豊田恵二郎 <keijiro@toyodakeijirounoiMac.local>
2017-10-29 04:18:26 +0900

    Modify header

--- app/webFront/lover-duck-web-front/src/components/Header.vue
+++ app/webFront/lover-duck-web-front/src/components/Header.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <p>
-      Lover Duck
-    </p>
+    <img src="../assets/logo_horizontal.svg" alt="lover duck">
   </div>
 </template>
 <style scoped>
-p {
-  
-}
+  div {
+    background-color: #ffe137;
+    height: 75px;
+  }
+
+  img {
+    height: 70%;
+    margin-left: 200px;
+    margin-top: 5px;
+  }
 </style>
