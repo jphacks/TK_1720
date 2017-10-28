da55f979e74ac235290b4bacecb0dd94f0e0e8d2
Yasu31 <yasu313nori@gmail.com>
2017-10-28 14:25:53 +0900

    一つのファイルに統合

--- app/inTheDuck/LoverDuck.ino
+++ app/inTheDuck/LoverDuck.ino
@@ -46,4 +46,37 @@ void sendLSMData(float data[]){
   mySend("z"+(String)data[2]);
 }
 
+void comBegin(int speed){
+  if(isBT){
+    //passkey is 1234
+    BT.begin(speed);
+  }
+  else{
+    Serial.begin(speed);
+  }
+}
+
+void mySend(String s){
+  //add device ID
+  s=idString+s;
+  if(isBT){
+    BT.println(s);
+  }
+  else{
+    Serial.println(s);
+  }
+}
+
+boolean comAvailable(){
+  boolean value;
+  if(isBT){
+    value=BT.available(); 
+  }
+  else{
+    value=Serial.available();
+  }
+  return value;
+}
+
+
 
