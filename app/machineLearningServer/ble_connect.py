02504e5cdef82560bc23246397f27938edcb4d41
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-10-29 10:34:37 +0900

    Create ble_connect.py

---
+++ app/machineLearningServer/ble_connect.py
@@ -0,0 +1,17 @@
+#coding utf-8
+import serial
+import re
+
+def main():
+    with serial.Serial('/dev/cu.HC-06-DevB', 9600, timeout=1) as ser:
+        while True:
+            c = ser.readline()
+            d = re.findall('[0-9]+\.+[0-9]',str(c),flags=0)
+            d = [float(i) for i in d]
+            for i in range(0, len(d)):  #要素を1つずつ順番に出力します
+                print(d[i])
+        ser.close()
+
+if __name__=="__main__":
+    main()
+
