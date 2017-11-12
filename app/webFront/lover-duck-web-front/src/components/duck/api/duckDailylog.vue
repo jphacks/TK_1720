efba312ff15e497004b89d9090dc29e1c8775cb9
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-11-12 11:10:36 +0900

    added reverse 2

--- app/webFront/lover-duck-web-front/src/components/duck/api/duckDailylog.vue
+++ app/webFront/lover-duck-web-front/src/components/duck/api/duckDailylog.vue
@@ -9,7 +9,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="log in list">
+        <tr v-for="log in list.reverse()">
           <th scope="row">{{ list.indexOf(log) + 1 }}</th>
           <td>{{ log.startAt }}</td>
           <td>{{ log.endAt }}</td>
